415841159d1360c14a07d98b67a87222475f21e4
Bob.Zhao <bob.zhao@infomax.com.tw>
2012-09-12 11:10:21 +0800

    add shortcut for export PATHs Signed-off-by: Bob.Zhao <bob.zhao@infomax.com.tw>

---
+++ lib/shortcut.zsh
@@ -0,0 +1,16 @@
+
+function a2path() {
+    export PATH=/opt/android2.2/out/host/linux-x86/bin:$PATH
+    echo $PATH
+}
+
+function a4path() {
+    export PATH=/opt/android4.0/out/host/linux-x86/bin:$PATH
+    echo $PATH
+}
+
+function asdkpath() {
+    export PATH=/opt2/sdk/android-sdk-linux/tools:/opt2/sdk/android-sdk-linux/platform-tools:$PATH
+    echo $PATH
+}
+
