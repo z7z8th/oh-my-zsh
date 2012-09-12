3c39dc119d0f612c61c69768a25b70438496873e
Bob.Zhao <bob.zhao@infomax.com.tw>
2012-09-12 11:09:36 +0800

    delete ms style  md/rd alias Signed-off-by: Bob.Zhao <bob.zhao@infomax.com.tw>

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -38,3 +38,4 @@ alias cda2a='cd /opt/android2.2-BSP12R_A1/'
 alias gae-proxy='~/Downloads/goagent/local/proxy.py'
 
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
