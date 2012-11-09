66ba0539b0f29503270c9d4be8c9c65584a2f4cd
Bob.Zhao <bob.zhao@infomax.com.tw>
2012-11-09 14:14:08 +0800

    add apt aliases

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -42,6 +42,7 @@ alias cda4='cd /opt/android4.0/'
 alias cda2='cd /opt/android2.2/'
 alias cda2a='cd /opt/android2.2-BSP12R_A1/'
 alias gae-proxy='~/Downloads/goagent/local/proxy.py'
+alias cdm='cd /opt/modiotek/'
 
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
