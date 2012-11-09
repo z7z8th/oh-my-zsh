6e573ccce3f040c59d594c5a367686fea93428aa
Bob.Zhao <bob.zhao@infomax.com.tw>
2012-11-09 14:14:08 +0800

    add apt aliases

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -26,16 +26,23 @@ alias sl=ls # often screw this up
 
 alias afind='ack-grep -il'
 
-alias apts='apt-cache search'
-alias aptsh='apt-cache show'
 alias apti='apt-get install'
+alias aptbi='apt-get -t squeeze-backports install'
+alias aptei='apt-get -t experimental install'
 alias aptu='apt-get update'
+alias aptug='apt-get upgrade'
+alias aptdug='apt-get dist-upgrade'
+alias apts='apt-cache search'
+alias aptpl='apt-cache policy'
+alias aptsh='apt-cache show'
+alias aptpl='apt-cache policy'
 
 alias bl='bash -l'
 alias cda4='cd /opt/android4.0/'
 alias cda2='cd /opt/android2.2/'
 alias cda2a='cd /opt/android2.2-BSP12R_A1/'
 alias gae-proxy='~/Downloads/goagent/local/proxy.py'
+alias cdm='cd /opt/modiotek/'
 
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
