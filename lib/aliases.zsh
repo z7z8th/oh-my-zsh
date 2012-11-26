3e20c9f38b45303bb9f3adf6723a1e9ca435e7ec
Bob.Zhao <bob.zhao@infomax.com.tw>
2012-11-26 10:06:58 +0800

    add alias bl='bash -l'

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -46,3 +46,4 @@ alias cdm='cd /opt/modiotek/'
 
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
+alias bl='bash -l'
