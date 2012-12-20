02109cb241d514672c6f1d1f60b3c626b3927208
Bob.Zhao <bob.zhao@infomax.com.tw>
2012-12-20 17:27:07 +0800

    plugin for java env

---
+++ lib/java.zsh
@@ -0,0 +1,5 @@
+export JAVA_HOME=/usr/lib/jvm/java-6-sun
+export JRE_HOME=$JAVA_HOME/jre
+export CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib:$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/dt.jar:$CLASSPATH 
+export PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin
+export ANDROID_JAVA_HOME=$JAVA_HOME
