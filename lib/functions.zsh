007b250687e77331764b2e79c3bdfc0e709dcdb3
Bob.Zhao <bob.zhao@infomax.com.tw>
2014-06-27 17:02:50 +0800

    change android src path in funtions utils

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -73,3 +73,28 @@ function env_default() {
     env | grep -q "^$1=" && return 0 
     export "$1=$2"       && return 3
 }
+
+function a2path() {
+    export PATH=/src/android2.2/out/host/linux-x86/bin:$PATH
+    echo $PATH
+}
+
+function a4path() {
+    export PATH=/src/android4.0/out/host/linux-x86/bin:$PATH
+    echo $PATH
+}
+
+function asdkpath() {
+    export PATH=/opt/sdk/android-sdk-linux/tools:/opt/sdk/android-sdk-linux/platform-tools:$PATH
+    echo $PATH
+}
+
+function armpath() {
+    export PATH=$HOME/Downloads/arm-2012.09/bin:$PATH
+    echo $PATH
+}
+
+function armgccpath() {
+    export PATH=/opt/toolchains/arm-eabi-4.6/bin:$PATH
+    echo $PATH
+}
