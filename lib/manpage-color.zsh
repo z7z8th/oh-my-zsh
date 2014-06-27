bfa7a4a1f0da5a69fab038e46e2948d381c4b832
Bob.Zhao <bob.zhao@infomax.com.tw>
2014-06-27 16:28:06 +0800

    colors for manpage

---
+++ lib/manpage-color.zsh
@@ -0,0 +1,9 @@
+
+# Less Colors for Man Pages
+export LESS_TERMCAP_mb=$'\E[1;31m'         # begin blinking
+export LESS_TERMCAP_md=$'\E[1;31;5m'       # begin bold
+export LESS_TERMCAP_me=$'\E[0m'            # end mode
+export LESS_TERMCAP_se=$'\E[0m'            # end standout-mode
+export LESS_TERMCAP_so=$'\E[5;0;40;1;32m'  # begin standout-mode - info box
+export LESS_TERMCAP_ue=$'\E[0m'            # end underline
+export LESS_TERMCAP_us=$'\E[4;5;1;34m'     # begin underline
